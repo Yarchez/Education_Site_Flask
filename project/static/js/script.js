3694c4f46e389e1f18f8f96887e2bc72f4dccaee
Yaroslav <156789916+Yarchez@users.noreply.github.com>
2024-05-09 00:47:15 +0300

    Delete project directory

--- project/static/js/script.js
+++
@@ -1,9 +0,0 @@
-function toggleVisibility(taskId) {
-  var answerDiv = document.getElementById('answer_' + taskId);
-  if (answerDiv.style.display === 'none') {
-    answerDiv.style.display = 'block';
-  } else {
-    answerDiv.style.display = 'none';
-  }
-}
-
