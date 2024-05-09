bb5177a9ba48318f34d6e3c9dcd5565d807004a4
Yaroslav <156789916+Yarchez@users.noreply.github.com>
2024-05-09 14:41:05 +0300

    Update config.py

--- db/config.py
+++ db/config.py
@@ -17,10 +17,10 @@ class Settings(BaseSettings):
 
 settings = Settings(
     DB_HOST="localhost",
-    DB_PORT=5432,
-    DB_USER="postgres",
-    DB_PASS="123",
-    DB_NAME="flask_db",
+    DB_PORT=YOUR_DB_PORT,
+    DB_USER="YOUR_DB_USER",
+    DB_PASS="YOUR_DB_PASS",
+    DB_NAME="YOUR_DB_NAME",
 )
 
 print(settings.DATABASE_URL)
